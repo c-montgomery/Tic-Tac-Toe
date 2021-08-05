f025b62be875eefe65dc0e0bf4d876edf036af88
c-montgomery <craym87@gmail.com>
2021-08-04 21:14:39 -0700

    added Gameboard module and player factory function

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,3 +1,44 @@
-const Gameboard = {
-    board: [0,1,2,3,4,5,6,7,8]
+const Gameboard = (() =>{
+    const main = document.querySelector('main')
+    const board = ['','','','','','','','',''];
+    const markSquare = function (e){
+        console.log(e)
+        let boardArray = document.querySelectorAll('div')
+        // e.target.innerText = current.marking
+    }
+    const createBoxes = () => {
+        let count = 0
+        board.forEach((box)=>{
+            let square = document.createElement('div');
+            square.addEventListener('click', Gameboard.markSquare)
+            square.textContent = `i${count}`;
+            square.style.lineHeight = '200px'
+            square.style.fontSize = '100px';
+            square.index = count;
+            Gameboard.board[count] = square
+            main.appendChild(square)
+            count++
+            
+        })
+    };
+    return{
+        board,
+        markSquare,
+        createBoxes,
+    }
+})()
+
+
+const Player = (playerName, symbol)=>{
+    // const currentTurn = Play.turn
+    const marking = symbol;
+    const name = playerName;
+    return {name, marking}
 }
+
+
+const player1 = Player('jeff', 'O')
+const player2 = Player('Gary', 'X')
+Gameboard.createBoxes()
+console.log(Player.currentPlayer)
+console.log(Gameboard.board[1])
